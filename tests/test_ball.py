92b97b6ffd5eb9158bc533b7ba42b5c1a39ca1bf
Sean <seanmcerlean82@gmail.com>
2020-01-08 22:01:51 +0000

    V0.2 - initial pytests

---
+++ tests/test_ball.py
@@ -0,0 +1,45 @@
+import random
+import ball
+
+def test_constructor():
+    x = random.randint(-100, 100)
+    y = random.randint(-100, 100)
+    speed = random.randint(-100, 100)
+    test_ball = ball.Ball(x, y, speed)
+
+    assert test_ball.xcor() == x
+    assert test_ball.ycor() == y
+    assert test_ball.x_speed == speed or test_ball.x_speed == -speed
+    assert test_ball.y_speed == speed or test_ball.y_speed == -speed
+    assert test_ball.shape() == 'square'
+    assert test_ball.color() == ('white', 'white')
+    assert test_ball.out_of_bounds_left == 0
+    assert test_ball.out_of_bounds_right == 0
+
+def test_move():
+    x = random.randint(-100, 100)
+    y = random.randint(-100, 100)
+    speed = random.randint(-100, 100)
+    test_ball = ball.Ball(x, y, speed)
+
+    test_ball.move()
+    assert test_ball.xcor() == x + speed
+    assert test_ball.ycor() == y + speed
+
+def test_reset_speed_with_a_positive_speed():
+        test_ball = ball.Ball(0, 0, 1)
+        test_ball.x_speed = 2
+        test_ball.y_speed = 3
+
+        test_ball.reset_speed()
+        assert test_ball.xcor() == -1
+        assert test_ball.ycor() == -1
+
+def test_reset_speed_with_a_negative_speed():
+        test_ball = ball.Ball(0, 0, 1)
+        test_ball.x_speed = -4
+        test_ball.y_speed = -5
+
+        test_ball.reset_speed()
+        assert test_ball.xcor() == 1
+        assert test_ball.ycor() == 1
---
+++ tests/test_scoreboard.py
@@ -0,0 +1,29 @@
+import random
+import scoreboard
+
+def test_constructor():
+    x = random.randint(-100, 100)
+    y = random.randint(-100, 100)
+    test_scoreboard = scoreboard.ScoreBoard(x, y)
+
+    assert test_scoreboard.xcor() == x
+    assert test_scoreboard.ycor() == y
+    assert test_scoreboard.color() == ('white', 'white')
+    assert test_scoreboard._player1_score == 0
+    assert test_scoreboard._player2_score == 0
+
+def test_add_player_1_score():
+    x = random.randint(-100, 100)
+    y = random.randint(-100, 100)
+    test_scoreboard = scoreboard.ScoreBoard(x, y)
+
+    test_scoreboard.add_player1_score()
+    assert test_scoreboard._player1_score == 1
+
+def test_add_player_1_score():
+    x = random.randint(-100, 100)
+    y = random.randint(-100, 100)
+    test_scoreboard = scoreboard.ScoreBoard(x, y)
+
+    test_scoreboard.add_player2_score()
+    assert test_scoreboard._player2_score == 1
